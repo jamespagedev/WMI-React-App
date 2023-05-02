30d9750401c0cac9bf689892c5ef5c1796cacf88
Vakon <jamespagedev@outlook.com>
2023-05-02 13:30:16 -0500

    refactor: table into component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
 import React from "react";
 import "./App.css";
 import data from "./honda_wmi.json";
-import { Header, View } from "./components";
+import { Header, Table, View } from "./components";
 
 function App() {
   const keys = ["Name", "WMI", "Country", "CreatedOn", "VehicleType"];
-
-  const getRowsJsx = () => {
-    return data.map((d) => {
-      const wmi = d.WMI;
-      return (
-        <tr key={wmi}>
-          {keys.map((k) => (
-            <td key={`${wmi}-${k}`}>{d[k]}</td>
-          ))}
-        </tr>
-      );
-    });
-  };
 
   return (
     <div className="App">
       <Header numOfTableRows={data.length} />
       <View>
-        <table className="m-auto">
-          <thead>
-            <tr>
-              {keys.map((k) => (
-                <th key={k}>{k}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>{getRowsJsx()}</tbody>
-        </table>
+        <Table columnNames={keys} rowsData={data} />
       </View>
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from "./Header.js";
 export { default as View } from "./View.js";
+export { default as Table } from "./Table.js";
