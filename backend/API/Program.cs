000e089f93ad967373631727ff36cdfd0c976f52
Vakon <jamespagedev@outlook.com>
2023-05-02 17:13:13 -0500

    fix: env setup

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -2,23 +2,24 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-if (builder.Environment.IsDevelopment())
+if (env == "Development")
 {
     builder.Services.AddDbContext<ApiDbContext>(opt => opt.UseInMemoryDatabase("PlatformsDbInMem"));
 }
 
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (env == "Development")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
